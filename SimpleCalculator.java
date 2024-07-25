0d0e12d8396315f0bb1e424df2a7aecbfc71711d
Niraj Kumar <114907754+nirajgopalpur51@users.noreply.github.com>
2024-07-25 09:23:13 +0530

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SimpleCalculator {
+public class SimpleCalculator 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
